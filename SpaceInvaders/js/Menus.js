040ea4d327f0f08dc9ebca4770b662c393b70170
Darkbriks <antoine.patron.62610@gmail.com>
2025-03-31 08:25:14 +0200

    Add a Classic Space Invaders in a secondary page

--- SpaceInvaders/js/Menus.js
+++ SpaceInvaders/js/Menus.js
@@ -10,6 +10,7 @@ function DisplayMainMenu()
     mainMenu.innerHTML = "<h1>Space Invaders</h1>"
     mainMenu.innerHTML += "<button onclick='DisplayLevelMenu()'>PLAY</button>"
     mainMenu.innerHTML += "<button>OPTIONS (soon)</button>"
+    mainMenu.innerHTML += "<button onclick='window.location.href=\"classicSpaceInvaders.html\"'>Classic Space Invaders</button>"
 }
 
 function DisplayLevelMenu()
